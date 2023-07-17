19a336d8b7dd756018b9af84caeca5dff6bdd0b8
AndreaWalchshoferSCCH <andrea.walchshofer@scch.at>
2023-07-17 14:45:33 +0200

    Remove accidentally committed build directory

--- build/Test2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/walchshofer/Documents/git/EmmyLuaCodeStyle/Test2
-# Build directory: C:/Users/walchshofer/Documents/git/EmmyLuaCodeStyle/build/Test2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
